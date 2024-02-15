b6585d381185e6a83e49dcceb3801f3579fc4c36
happyf1re <dmur1991@gmail.com>
2024-02-15 07:58:01 +0300

    Убрали работу с хедерами

--- src/main/java/com/altasoft/exchange/message/MessageConsumer.java
+++ src/main/java/com/altasoft/exchange/message/MessageConsumer.java
@@ -1,21 +1,13 @@
 package com.altasoft.exchange.message;
 
-import com.altasoft.exchange.user.User;
 import com.altasoft.exchange.user.UserRepository;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 
 @Service
 public class MessageConsumer {
@@ -26,66 +18,76 @@ public class MessageConsumer {
     private final UserRepository userRepository;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @Autowired
-    public MessageConsumer(MessageRepository messageRepository, UserRepository userRepository, KafkaTemplate<String, String> kafkaTemplate) {
+    private final ObjectMapper objectMapper;
+
+    public MessageConsumer(MessageRepository messageRepository, UserRepository userRepository, KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
         this.messageRepository = messageRepository;
         this.userRepository = userRepository;
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @KafkaListener(topics = "main-topic", groupId = "group-id")
     @Transactional
-    public void listenToMainTopic(ConsumerRecord<String, String> record) {
+    public void listenToMainTopic(String messageJsonStr) {
         LOGGER.info("Вошли в метод слушателя");
 
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode rootNode = mapper.readTree(record.value());
+//        try {
+//            ObjectMapper mapper = new ObjectMapper();
+//            JsonNode rootNode = mapper.readTree(record.value());
+//
+//            // Извлекаем и логируем необходимые данные
+//            String message = rootNode.path("message").asText();
+//            String authorUserName = rootNode.path("authorUserName").asText();
+//            String recipientUserName = rootNode.path("recipientUserName").asText();
+//            LOGGER.info("Содержимое сообщения: message ----> {}, authorUserName ----> {}, recipientUserName ----> {}",
+//                    message, authorUserName, recipientUserName);
+//        } catch (Exception e) {
+//            LOGGER.error("Ошибка при обработке JSON сообщения: {}", e.getMessage());
+//        }
 
-            // Извлекаем и логируем необходимые данные
-            String message = rootNode.path("message").asText();
-            String authorUserName = rootNode.path("authorUserName").asText();
-            String recipientUserName = rootNode.path("recipientUserName").asText();
-            LOGGER.info("Содержимое сообщения: message ----> {}, authorUserName ----> {}, recipientUserName ----> {}",
-                    message, authorUserName, recipientUserName);
+
+//        // Извлечение имени пользователя-получателя и автора из хедера сообщения
+//        String recipientUserName = new String(record.headers().lastHeader("recipientUserName").value(), StandardCharsets.UTF_8);
+//        String authorUserName = new String(record.headers().lastHeader("authorUserName").value(), StandardCharsets.UTF_8);
+//
+//        // Выводим полную информацию о сообщении в лог
+//        LOGGER.info("Получено сообщение: authorName ----> {}, recipientName -----> {}, message -----> {}",
+//                authorUserName, recipientUserName, record.value());
+
+        try {
+            MessageJson messageJson = objectMapper.readValue(messageJsonStr, MessageJson.class);
+            // Логика определения целевого топика на основе имени получателя
+            String targetTopic = messageJson.getRecipientUserName() + "-topic";
+            kafkaTemplate.send(targetTopic, messageJsonStr);
         } catch (Exception e) {
-            LOGGER.error("Ошибка при обработке JSON сообщения: {}", e.getMessage());
+            // Обработка ошибок
         }
 
 
-        // Извлечение имени пользователя-получателя и автора из хедера сообщения
-        String recipientUserName = new String(record.headers().lastHeader("recipientUserName").value(), StandardCharsets.UTF_8);
-        String authorUserName = new String(record.headers().lastHeader("authorUserName").value(), StandardCharsets.UTF_8);
-
-        // Выводим полную информацию о сообщении в лог
-        LOGGER.info("Получено сообщение: authorName ----> {}, recipientName -----> {}, message -----> {}",
-                authorUserName, recipientUserName, record.value());
-
-
-
-        User recipient = userRepository.findByUserName(recipientUserName)
-                .orElseThrow(() -> new RuntimeException("Recipient user not found: " + recipientUserName));
-
-        // Поиск пользователя-автора в базе данных
-        User author = userRepository.findByUserName(authorUserName)
-                .orElseThrow(() -> new RuntimeException("Author user not found: " + authorUserName));
-
-        // Создание и сохранение сообщения в базе данных
-        Message message = new Message();
-        message.setAuthor(author);
-        message.setRecipient(recipient);
-        message.setContent(record.value());
-        message.setTimestamp(LocalDateTime.now());
-        messageRepository.save(message);
-
-        // Отправка сообщения в топик, соответствующий пользователю-получателю
-        String targetTopic = recipientUserName + "-topic";
-        try {
-            kafkaTemplate.send(targetTopic, record.value());
-            LOGGER.info("Message sent to topic: {}", targetTopic);
-        } catch (KafkaException e) {
-            LOGGER.error("Failed to send message to topic: {}. Error: {}", targetTopic, e.getMessage());
-            // Здесь можно добавить дополнительную логику обработки ошибки, например, повторную отправку или уведомление администратора
-        }
+//        User recipient = userRepository.findByUserName(recipientUserName)
+//                .orElseThrow(() -> new RuntimeException("Recipient user not found: " + recipientUserName));
+//
+//        // Поиск пользователя-автора в базе данных
+//        User author = userRepository.findByUserName(authorUserName)
+//                .orElseThrow(() -> new RuntimeException("Author user not found: " + authorUserName));
+//
+//        // Создание и сохранение сообщения в базе данных
+//        Message message = new Message();
+//        message.setAuthor(author);
+//        message.setRecipient(recipient);
+//        message.setContent(record.value());
+//        message.setTimestamp(LocalDateTime.now());
+//        messageRepository.save(message);
+//
+//        // Отправка сообщения в топик, соответствующий пользователю-получателю
+//        String targetTopic = recipientUserName + "-topic";
+//        try {
+//            kafkaTemplate.send(targetTopic, record.value());
+//            LOGGER.info("Message sent to topic: {}", targetTopic);
+//        } catch (KafkaException e) {
+//            LOGGER.error("Failed to send message to topic: {}. Error: {}", targetTopic, e.getMessage());
+//            // Здесь можно добавить дополнительную логику обработки ошибки, например, повторную отправку или уведомление администратора
+//        }
     }
 }
--- src/main/java/com/altasoft/exchange/message/MessageProducer.java
+++ src/main/java/com/altasoft/exchange/message/MessageProducer.java
@@ -5,12 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
-
-import java.nio.charset.StandardCharsets;
 
 @Service
 public class MessageProducer {
@@ -25,25 +20,38 @@ public class MessageProducer {
     }
 
     public void sendMessage(String topic, MessageJson messageContent) {
-        try {
-            // Пробуем сериализовать объект MessageJson в строку JSON
-            String jsonMessage = objectMapper.writeValueAsString(messageContent);
-            // Если сериализация прошла успешно, логируем это
-            LOGGER.info("=======================Сообщение успешно сериализовано===========================");
-            LOGGER.info("Сериализованное сообщение: {}", jsonMessage);
 
-            // Отправляем сообщение в Kafka
-            Message<String> message = MessageBuilder
-                    .withPayload(jsonMessage)
-                    .setHeader(KafkaHeaders.TOPIC, topic)
-                    .setHeader("authorUserName", messageContent.getAuthorUserName().getBytes(StandardCharsets.UTF_8))
-                    .setHeader("recipientUserName", messageContent.getRecipientUserName().getBytes(StandardCharsets.UTF_8))
-                    .build();
-            kafkaTemplate.send(message);
-            LOGGER.info("=======================Сообщение отправлено в кафку===========================");
-        } catch (JsonProcessingException e) {
-            // Логируем ошибку, если сериализация не удалась
-            LOGGER.error("Ошибка при сериализации сообщения в JSON: {}", e.getMessage());
-        }
+            try {
+                // Сериализация объекта MessageJson в строку JSON
+                String jsonMessage = objectMapper.writeValueAsString(messageContent);
+                // Логирование отправки сообщения
+                LOGGER.info("Сообщение отправляется в кафку: {}", jsonMessage);
+                // Отправка сериализованного сообщения в указанный топик
+                kafkaTemplate.send(topic, jsonMessage);
+                LOGGER.info("Сообщение успешно отправлено в топик: {}", topic);
+            } catch (JsonProcessingException e) {
+                LOGGER.error("Ошибка при сериализации сообщения в JSON: {}", e.getMessage());
+            }
+
+//        try {
+//            // Пробуем сериализовать объект MessageJson в строку JSON
+//            String jsonMessage = objectMapper.writeValueAsString(messageContent);
+//            // Если сериализация прошла успешно, логируем это
+//            LOGGER.info("=======================Сообщение успешно сериализовано===========================");
+//            LOGGER.info("Сериализованное сообщение: {}", jsonMessage);
+//
+//            // Отправляем сообщение в Kafka
+//            Message<String> message = MessageBuilder
+//                    .withPayload(jsonMessage)
+//                    .setHeader(KafkaHeaders.TOPIC, topic)
+//                    .setHeader("authorUserName", messageContent.getAuthorUserName().getBytes(StandardCharsets.UTF_8))
+//                    .setHeader("recipientUserName", messageContent.getRecipientUserName().getBytes(StandardCharsets.UTF_8))
+//                    .build();
+//            kafkaTemplate.send(message);
+//            LOGGER.info("=======================Сообщение отправлено в кафку===========================");
+//        } catch (JsonProcessingException e) {
+//            // Логируем ошибку, если сериализация не удалась
+//            LOGGER.error("Ошибка при сериализации сообщения в JSON: {}", e.getMessage());
+//        }
     }
 }
