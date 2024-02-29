812938c4f3bc76c322a554964f27d02b03c8e54c
happyf1re <dmur1991@gmail.com>
2024-02-29 16:12:22 +0300

    Изменена конфигурация, заккоментированы некоторые классы

--- src/main/java/com/altasoft/exchange/message/Message.java
+++ src/main/java/com/altasoft/exchange/message/Message.java
@@ -1,37 +1,37 @@
-package com.altasoft.exchange.message;
-
-import com.altasoft.exchange.user.User;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.LocalDateTime;
-
-@Entity
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name = "_messages")
-public class Message {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User author;
-
-    //добавим получателя, его мы берём из хедера кафки
-    @ManyToOne
-    @JoinColumn(name = "recipient_id")
-    private User recipient;
-
-    @Column(nullable = false)
-    private String content;
-
-    @Column(nullable = false)
-    private LocalDateTime timestamp;
-
-}
+//package com.altasoft.exchange.message;
+//
+//import com.altasoft.exchange.user.User;
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Getter
+//@Setter
+//@ToString
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Table(name = "_messages")
+//public class Message {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer id;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private User author;
+//
+//    //добавим получателя, его мы берём из хедера кафки
+//    @ManyToOne
+//    @JoinColumn(name = "recipient_id")
+//    private User recipient;
+//
+//    @Column(nullable = false)
+//    private String content;
+//
+//    @Column(nullable = false)
+//    private LocalDateTime timestamp;
+//
+//}
--- src/main/java/com/altasoft/exchange/message/MessageConsumer.java
+++ src/main/java/com/altasoft/exchange/message/MessageConsumer.java
@@ -1,47 +1,47 @@
-package com.altasoft.exchange.message;
-
-import com.altasoft.exchange.user.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.transaction.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MessageConsumer {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
-
-    private final MessageRepository messageRepository;
-    private final UserRepository userRepository;
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
-    private final ObjectMapper objectMapper;
-
-    public MessageConsumer(MessageRepository messageRepository, UserRepository userRepository, KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
-        this.messageRepository = messageRepository;
-        this.userRepository = userRepository;
-        this.kafkaTemplate = kafkaTemplate;
-        this.objectMapper = objectMapper;
-    }
-
-    @KafkaListener(topics = "main-topic", groupId = "group-id")
-    @Transactional
-    public void listenToMainTopic(String messageJsonStr) {
-        LOGGER.info("Вошли в метод слушателя");
-
-
-        try {
-            MessageJson messageJson = objectMapper.readValue(messageJsonStr, MessageJson.class);
-            // Логика определения целевого топика на основе имени получателя
-            String targetTopic = messageJson.getRecipientUserName() + "-topic";
-            kafkaTemplate.send(targetTopic, messageJsonStr);
-        } catch (Exception e) {
-            // Обработка ошибок
-        }
-
-
-    }
-}
+//package com.altasoft.exchange.message;
+//
+//import com.altasoft.exchange.user.UserRepository;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import jakarta.transaction.Transactional;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MessageConsumer {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+//
+//    private final MessageRepository messageRepository;
+//    private final UserRepository userRepository;
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//
+//    private final ObjectMapper objectMapper;
+//
+//    public MessageConsumer(MessageRepository messageRepository, UserRepository userRepository, KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
+//        this.messageRepository = messageRepository;
+//        this.userRepository = userRepository;
+//        this.kafkaTemplate = kafkaTemplate;
+//        this.objectMapper = objectMapper;
+//    }
+//
+//    @KafkaListener(topics = "main-topic", groupId = "group-id")
+//    @Transactional
+//    public void listenToMainTopic(String messageJsonStr) {
+//        LOGGER.info("Вошли в метод слушателя");
+//
+//
+//        try {
+//            MessageJson messageJson = objectMapper.readValue(messageJsonStr, MessageJson.class);
+//            // Логика определения целевого топика на основе имени получателя
+//            String targetTopic = messageJson.getRecipientUserName() + "-topic";
+//            kafkaTemplate.send(targetTopic, messageJsonStr);
+//        } catch (Exception e) {
+//            // Обработка ошибок
+//        }
+//
+//
+//    }
+//}
