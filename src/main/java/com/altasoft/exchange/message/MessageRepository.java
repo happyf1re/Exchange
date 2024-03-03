cca8360a4f21c0dd850b4188462fbc8de27482f7
happyf1re <dmur1991@gmail.com>
2024-03-03 17:45:36 +0300

    Удалены лишние классы, теперь образ собирается корректно

--- src/main/java/com/altasoft/exchange/message/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.altasoft.exchange.message;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<Message, Integer> {
-}
