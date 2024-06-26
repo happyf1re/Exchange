cfc70bd29abd00244845ff7e6641ae7f76dedb5b
happyf1re <dmur1991@gmail.com>
2024-06-26 11:17:01 +0300

    Корректное отображение фронта

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://exfront.a-m0.ru/api/v1',
+    baseURL: 'https://exchange.a-m0.ru/api/v1',
     withCredentials: true,
 });
 
